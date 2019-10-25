6a9ef7e9e2adc1d8dd79d933e9899371773131ae
Matteo AUGER <augermtt@gmail.com>
2019-10-25 13:55:02 +0200

    Update main to match last commit

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
-	matched, message := worker.Match("test", "098f6bcd4621d373cade4e832627b4f6")
-	fmt.Println(matched, message)
+	testStr := "test"
+	matched := worker.Match(testStr, "098f6bcd4621d373cade4e832627b4f6")
+	fmt.Println(matched, testStr)
 }
