dfd284ce4e152313917af5e272e7e849004b6fb6
Yoann Eichelberger <eichelberger.y@gmail.com>
2020-01-26 22:27:47 +0100

    Add worker call from master

--- cmd/hash-n-go/main.go
+++ cmd/hash-n-go/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"os/exec"
 )
 
 func main() {
-	fmt.Println("I'm the master.")
+	worker := "gitlab.com/hacheurs/hash-n-go/cmd/hash-n-go-worker"
+	start := "000"
+	end := "999"
+	target := "111"
+	hash := md5Hash(target)
+	cmd := exec.Command("go", "run", worker, start, end, hash)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Error %v\n", err)
+	} else {
+		fmt.Printf("Output: %s\n", out)
+	}
+}
+
+func md5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
