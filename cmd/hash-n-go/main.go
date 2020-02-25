2f116d3a59c6b88d0cc5037b080a865d1eda09bb
Matteo AUGER <matteo.auger@etu.univ-lehavre.fr>
2020-02-25 22:50:32 +0100

    Refactor node count function

--- cmd/hash-n-go/main.go
+++ cmd/hash-n-go/main.go
@@ -2,98 +2,52 @@ package main
 
 import (
 	"fmt"
-    "bytes"
-    "io"
-    "os"
-	"os/exec"
-    "strconv"
-    "strings"
+	"os"
+	"strconv"
+
+	"gitlab.com/hacheurs/hash-n-go/pkg/system/swarm"
 )
 
-// TODO instructions suivantes : 
+// TODO instructions suivantes :
 // Args : ["hash", "websocket-URI"]
 // Récupérer n machines slaves
-// Subdiviser l'espace de recherche avec n 
+// Subdiviser l'espace de recherche avec n
 // Distribuer le travail aux workers
 // On return : envoi websocket au serveur node
 
-// 26 * 2 + 10 = 62 * 6 = 372 caractères 
+// 26 * 2 + 10 = 62 * 6 = 372 caractères
 
 func main() {
-    args := os.Args
+	args := os.Args
 
-    if len(args) < 3 || len(args) > 4 {
-        hint := "<hash> <websocket-URI> <optionnal: worker count>"
-        fmt.Printf("USAGE : %s %s \n", args[0], hint)
-        os.Exit(1)
-    }
+	if len(args) < 3 || len(args) > 4 {
+		hint := "<hash> <websocket-URI> <optionnal: worker count>"
+		fmt.Printf("USAGE : %s %s \n", args[0], hint)
+		os.Exit(1)
+	}
 
-    //hash         := args[1]
-    //websocketUri := args[2]
+	//hash         := args[1]
+	//websocketUri := args[2]
 
-    // getting the worker count either from args or automatic
-    var nWorkers int
-    if len(args) > 3 {
-        var err error
-        nWorkers, err = strconv.Atoi(args[3])
+	// getting the worker count either from args or automatic
+	var nWorkers int
+	if len(args) > 3 {
+		var err error
+		nWorkers, err = strconv.Atoi(args[3])
 
-        if err != nil {
-            fmt.Printf("arg error : Worker count should be an integer")
-            os.Exit(1)
-        }
-    } else {
-        nWorkers = getNodeCount()
-    }
+		if err != nil {
+			fmt.Printf("arg error : Worker count should be an integer")
+			os.Exit(1)
+		}
+	} else {
+		nWorkers = swarm.GetNodeCount()
+	}
 
-    fmt.Println(nWorkers)
-    //fmt.Printf("%s %s", hash, websocketUri)
-}
-
-func getNodeCount() int {
-        //create command
-        ncCmd := exec.Command("docker", "node", "ls")
-        wcCmd := exec.Command("wc", "-l")
-
-        //make a pipe
-        reader, writer := io.Pipe()
-        var buf bytes.Buffer
-
-        //set the output of "node ls" command to pipe writer
-        ncCmd.Stdout = writer
-        //set the input of the "wc" command pipe reader
-
-        wcCmd.Stdin = reader
-
-        //cache the output of "wc" to memory
-        wcCmd.Stdout = &buf
-
-        //start to execute "node ls" command
-        ncCmd.Start()
-
-        //start to execute "wc" command
-        wcCmd.Start()
-
-        ncCmd.Wait()
-        writer.Close()
-
-        wcCmd.Wait()
-        reader.Close()
-
-        strStdout := string(buf.Bytes())
-        firstLine := strings.Split(strStdout, "\n")[0]
-        res, err := strconv.Atoi(firstLine)
-
-        if err != nil {
-            panic(err)
-        }
-
-        // return number of available workers
-        // minus 1 bc ignoring header
-        return res - 1
+	fmt.Println(nWorkers)
+	//fmt.Printf("%s %s", hash, websocketUri)
 }
 
 //func scaleWorkers() {
-    //var nbAvailableSlaves: int64 = 6
-    //var workAmmount : int64 = 372 / nbAvailableSlaves
+//var nbAvailableSlaves: int64 = 6
+//var workAmmount : int64 = 372 / nbAvailableSlaves
 //}
-
---
+++ pkg/system/swarm/swarm.go
@@ -0,0 +1,53 @@
+package swarm
+
+import (
+	"bytes"
+	"io"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+// GetNodeCount gets the docker swarm nodes count
+func GetNodeCount() int {
+	//create command
+	ncCmd := exec.Command("docker", "node", "ls")
+	wcCmd := exec.Command("wc", "-l")
+
+	//make a pipe
+	reader, writer := io.Pipe()
+	var buf bytes.Buffer
+
+	//set the output of "node ls" command to pipe writer
+	ncCmd.Stdout = writer
+	//set the input of the "wc" command pipe reader
+
+	wcCmd.Stdin = reader
+
+	//cache the output of "wc" to memory
+	wcCmd.Stdout = &buf
+
+	//start to execute "node ls" command
+	ncCmd.Start()
+
+	//start to execute "wc" command
+	wcCmd.Start()
+
+	ncCmd.Wait()
+	writer.Close()
+
+	wcCmd.Wait()
+	reader.Close()
+
+	strStdout := string(buf.Bytes())
+	firstLine := strings.Split(strStdout, "\n")[0]
+	res, err := strconv.Atoi(firstLine)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// return number of available workers
+	// minus 1 bc ignoring header
+	return res - 1
+}
