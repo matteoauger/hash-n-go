97f70ed25fda3d600d7066fc46a235d533801fee
Matteo AUGER <matteo.auger@etu.univ-lehavre.fr>
2020-02-29 22:34:45 +0100

    Use connHandler in client

--- pkg/net/ws/cli/wscli.go
+++ pkg/net/ws/cli/wscli.go
@@ -1,30 +1,18 @@
 package cli
 
 import (
-	"fmt"
 	"log"
-	"net/url"
 
 	"github.com/gorilla/websocket"
+	"gitlab.com/hacheurs/hash-n-go/pkg/net/ws"
 )
 
-func main() {
-	var url = url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/"}
-
-	ws, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
+// Connect connects to the websocket
+func Connect(url string, connHandler ws.ConnHandler) {
+	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
 
-	ws.WriteMessage(websocket.TextMessage, []byte("Hello world"))
-
-	for {
-		_, message, err := ws.ReadMessage()
-		if err != nil {
-			log.Println("read: ", err)
-			return
-		}
-
-		fmt.Println("received :", string(message))
-	}
+	connHandler(ws)
 }
