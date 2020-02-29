3c1ac20ab9739b16babf6bdab6f7af658aacb932
Matteo AUGER <matteo.auger@etu.univ-lehavre.fr>
2020-02-29 22:30:42 +0100

    Remove message handler as conn handler can do anything

--- pkg/net/ws/listener.go
+++ pkg/net/ws/listener.go
@@ -4,6 +4,3 @@ import "github.com/gorilla/websocket"
 
 // ConnHandler websocket connection listener function
 type ConnHandler func(*websocket.Conn)
-
-// MsgHandler websocket message listener function
-type MsgHandler func(string)
