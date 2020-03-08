1fb8d9fc15159dc4a04a777eeb81ea4de6d5019f
Matteo AUGER <matteo.auger@etu.univ-lehavre.fr>
2020-03-08 16:52:37 +0100

    Refactor: rename scaleworkers to scaleworkload

--- pkg/scal/scal.go
+++ pkg/scal/scal.go
@@ -1,45 +1,43 @@
 package scal
 
 import (
-    "math"
+	"math"
 
-    "gitlab.com/hacheurs/hash-n-go/pkg/char"
+	"gitlab.com/hacheurs/hash-n-go/pkg/char"
 )
 
+// ScaleWorkload returns the scaled work for the given number of wokers & digits
+func ScaleWorkload(nbWorkers int, nDigit int, hash string) []SearchSpace {
+	chars := char.CreateAlphabet()
+	base := len(chars)
+	nChars := int(math.Pow(float64(base), float64(nDigit))) - 1
+	workAmount := nChars / nbWorkers
+	schSpaces := make([]SearchSpace, nbWorkers)
 
-// ScaleWorkers returns the scaled work for the given number of wokers & digits 
-func ScaleWorkers(nbWorkers int, nDigit int, hash string) []SearchSpace {
-    chars  := char.CreateAlphabet()
-    base   := len(chars)
-    nChars := int(math.Pow(float64(base), float64(nDigit))) - 1
-    workAmount := nChars / nbWorkers
-    schSpaces := make([]SearchSpace, nbWorkers)
+	for i := 0; i < nbWorkers; i++ {
+		// calculate the begin string and end string for the given worker
+		beginIdx := (workAmount * i) + 1 // begin index
+		endIdx := workAmount * (i + 1)   // end index
+		beginStr := convertBase(beginIdx, chars)
+		endStr := convertBase(endIdx, chars)
 
-    for i := 0; i < nbWorkers; i++ {
-        // calculate the begin string and end string for the given worker
-        beginIdx := (workAmount * i) + 1  // begin index
-        endIdx   := workAmount * (i + 1)  // end index
-        beginStr := convertBase(beginIdx, chars)
-        endStr   := convertBase(endIdx,   chars)
+		schSpaces[i] = SearchSpace{Begin: beginStr, End: endStr, Hash: hash}
+	}
 
-        schSpaces[i] =  SearchSpace { Begin: beginStr, End: endStr, Hash: hash }
-    }
-
-    return schSpaces
+	return schSpaces
 }
 
-// convertBase converts the base of the given decimal number 
+// convertBase converts the base of the given decimal number
 func convertBase(nbr int, symbol []rune) string {
-    newBase := len(symbol)
-    res     := ""
+	newBase := len(symbol)
+	res := ""
 
-    n := nbr
+	n := nbr
 
-    for n > 0 {
-        res = string(symbol[n % newBase]) + res
-        n = n / newBase
-    }
+	for n > 0 {
+		res = string(symbol[n%newBase]) + res
+		n = n / newBase
+	}
 
-    return res
+	return res
 }
-
