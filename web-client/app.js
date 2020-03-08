8983c32300d335752282f31b008d691045754b57
Yoann Eichelberger <eichelberger.y@gmail.com>
2020-03-08 16:59:01 +0100

    Change html to hbs view template and fix ejs error

--- web-client/app.js
+++ web-client/app.js
@@ -1,17 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
-
+const app = express();
 
 // view engine setup
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
--- web-client/routes/index.js
+++ web-client/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hash');
+  res.render('index', { 
+    title: 'Hash-n-go',
+    time: {
+      selected: 60,
+      options: [60, 50, 30, 20, 10, 5, 1, 0.5, 0.1]
+    }
+  });
 });
 
 module.exports = router;
